4c2c0030112dbb4201c4e9bcb5bc0dd27ec5dfa7
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-11-28 01:41:41 +0530

    feat: add loader component with animated slices and styles

---
+++ components/ui/loader.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className="container">
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+        <div className="slice"></div>
+    </div>
+  )
+}
+
+export default Loader
+
+
+
+
+
