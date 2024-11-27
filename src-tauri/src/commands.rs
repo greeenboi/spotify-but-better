f558c5833edbe841d906776444134bec03dfdc3d
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-11-28 01:41:22 +0530

    feat: enhance link opening functionality, update dependencies, and add logging support

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,4 +1,5 @@
 #[tauri::command]
 pub fn open_link_on_click(url: &str) -> Result<(), String> {
+    log::info!("Opening link: {}", url);
     webbrowser::open(url).map_err(|e| e.to_string())
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,18 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-
 mod commands;
 
 pub fn run() {
     tauri::Builder::default()
-        
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
                         .level(log::LevelFilter::Info)
+                        .level(log::LevelFilter::Error)
+                        .level(log::LevelFilter::Warn)
                         .build(),
                 )?;
             }
